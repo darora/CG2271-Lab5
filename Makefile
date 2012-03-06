ea2ae17d8bec8516352784908fe783c8805e1940
Divyanshu Arora <div@linuxnus.org>
2012-03-06 18:14:59 +0800

    That was a dumb syntax to use in the makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
-all:
-	make clean
+all: clean mutex
 	gcc lab1.c -o lab1 -pthread
 	gcc lab.c -o lab -pthread
-	make mutex
 clean:
 	rm -f lab1
 	rm -f lab
--- lab5b.c
+++ lab5b.c
@@ -6,14 +6,17 @@
 int glob;
 
 pthread_t thread[NUM_THREADS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *child(void *t)
 {
 	// Increment glob by 1, wait for 1 second, then increment by 1 again.
 	printf("Child %d entering. Glob is currently %d\n", t, glob);
+    pthread_mutex_lock(&mutex);
 	glob++;
 	sleep(1);
 	glob++;
+    pthread_mutex_unlock(&mutex);
 	printf("Child %d exiting. Glob is currently %d\n", t, glob);
 }
 
@@ -31,6 +34,7 @@ int main()
         if (pthread_join(thread[i], NULL))
             return 2;
 	printf("Final value of glob is %d\n", glob);
+    pthread_mutex_destroy(&mutex);
 	return 0;
 }
 
